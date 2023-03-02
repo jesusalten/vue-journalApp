355e7865557844946e44e6c12a51156ef8c2442f
Jesus Alcantara <jesus.alcantara@alten.es>
2023-03-02 16:46:53 +0100

    update crud

--- src/modules/daybook/components/Fab.vue
+++ src/modules/daybook/components/Fab.vue
@@ -1,6 +1,9 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <button class="btn btn-primary">
+  <button 
+    class="btn btn-primary"
+    @click="$emit('on:click')"
+  >
       <i class="fa fa-2x"
           :class="icon"
       ></i>
--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,7 +1,22 @@
 <template>
     <Navbar />
 
-    <div class="d-flex">
+    <div 
+        v-if="isLoading"
+        class="row justify-content-md-center"
+    >
+        <div class="col-3 alert-info text-center mt-5">
+            Espere por favor...
+            <h3 class="mt-2">
+                <i class="fa fa-spin fa-sync"></i>
+            </h3>
+        </div>
+    </div>
+
+    <div 
+        v-else
+        class="d-flex"
+    >
         <div class="col-4">
             <EntryList />
         </div>
@@ -15,7 +30,7 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 
 export default {
     components: {
@@ -24,6 +39,9 @@ export default {
     },
     methods: {
         ...mapActions('journal', ['loadEntries'])
+    },
+    computed: {
+        ...mapState('journal', ['isLoading'])
     },
     created () {
         this.loadEntries()
